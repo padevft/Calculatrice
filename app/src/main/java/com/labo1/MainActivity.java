71680fa9e92e7138721985fa987fff492317e0b6
Paved <padev.ft@gamil.com>
2023-10-06 17:32:03 +0100

    Interaction sur les operandes

--- app/src/main/java/com/labo1/MainActivity.java
+++ app/src/main/java/com/labo1/MainActivity.java
@@ -4,16 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView inputTextView, resultTextView;
-    private boolean divideByZero = false, isNewInput = true;
+    private EditText inputTextView, inputTextViewOperation;
+    private boolean divideByZero = false, isNewInput = true, onClickEqual = false;
     private double currentResult = 0;
-    private String currentValue = "", currentOperator = "";
+    private String currentOperator = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,68 +22,89 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         inputTextView = findViewById(R.id.editText);
-        resultTextView = findViewById(R.id.result);
+        inputTextViewOperation = findViewById(R.id.editTextOperation);
     }
 
 
     public void onNumberClick(View view) {
         if (isNewInput) {
             inputTextView.setText("");
+            if (onClickEqual) inputTextViewOperation.setText("");
             isNewInput = false;
         }
 
         String number = ((TextView) view).getText().toString();
         String currentText = inputTextView.getText().toString();
 
-        if (!currentText.equals("0")) {
-            if (number.equals(",") && !currentValue.contains(".")) {
-                currentValue += ".";
+        if (!currentText.equals("0") && !currentText.equals("")) {
+            if (!number.equals(",") || !currentText.contains(",")) {
                 currentText += number;
-            } else if (number.equals(",") && currentValue.contains(".")) {
-                currentText = currentText;
-                currentValue = currentValue;
-            } else {
-                currentValue += number;
-                currentText += number;
-
             }
-
         } else {
-            currentValue = number;
-            currentText = number;
+            if (number.equals(",")) {
+                currentText = "0" + number;
+            } else {
+                currentText = number;
+            }
         }
-
-
         inputTextView.setText(currentText);
-        if (!currentOperator.equals("")) {
-            calculateResult(currentValue);
-            displayResult(false, divideByZero);
-        }
     }
 
-    public void onOperatorClick(View view) {
+    public void onClickOperatorTwoValue(View view) {
         String operator = ((TextView) view).getText().toString();
         String inputText = inputTextView.getText().toString();
-
+        currentOperator = operator;
+        onClickEqual = false;
         if (!isNewInput) {
-            if (currentOperator.equals(""))
-                currentResult = Double.parseDouble(currentValue);
-            currentValue = "";
-            currentOperator = operator;
-            inputTextView.setText(String.format("%s%s", inputText, operator));
+            currentResult = Double.parseDouble(splitJoinResult(inputText, ",", "."));
+            inputTextViewOperation.setText(String.format("%s%s", inputText, operator));
+            isNewInput = true;
+        } else {
+            String re = splitJoinResult(String.valueOf(currentResult), "\\.", ",");
+            inputTextViewOperation.setText(String.format("%s%s", re, operator));
         }
 
     }
 
-    public void onOperator2Click(View view) {
+
+    public void onClickOperatorOneValue(View view) {
+        String operator = ((TextView) view).getText().toString();
+        String inputText = inputTextView.getText().toString();
+        double value = Double.parseDouble(splitJoinResult(inputText, ",", "."));
+        switch (operator) {
+            case "±":
+                currentResult = value * (-1);
+                inputTextView.setText(splitJoinResult(String.valueOf(currentResult), "\\.", ","));
+                break;
+            case "√":
+                if (value > 0) {
+                    currentResult = Math.sqrt(value);
+                    inputTextView.setText(splitJoinResult(String.valueOf(currentResult), "\\.", ","));
+                } else {
+                    inputTextView.setText(R.string.dataNotValid);
+                }
+                break;
+            case "%":
+                currentResult = value % 2;
+                inputTextView.setText(splitJoinResult(String.valueOf(currentResult), "\\.", ","));
+                break;
+            case "1/x":
+                currentResult = 1 / value;
+                inputTextView.setText(splitJoinResult(String.valueOf(currentResult), "\\.", ","));
+                break;
+        }
+        isNewInput = true;
+    }
+
+
+    public void onClickOperatorClean(View view) {
         String operator = ((TextView) view).getText().toString();
         String inputText = inputTextView.getText().toString();
         switch (operator) {
             case "C":
             case "CE":
                 inputTextView.setText("0");
-                resultTextView.setText("");
-                currentValue = "";
+                inputTextViewOperation.setText("");
                 currentOperator = "";
                 currentResult = 0;
                 divideByZero = false;
@@ -99,47 +121,58 @@ public class MainActivity extends AppCompatActivity {
 
     public void onEqualClick(View view) {
         if (!isNewInput) {
-            displayResult(true, divideByZero);
+            calculateResult();
+            displayResult(divideByZero);
+            isNewInput = true;
+            onClickEqual = true;
         }
     }
 
 
-    private void calculateResult(String number) {
-        double value = Double.parseDouble(number);
+    private void calculateResult() {
+        double currentValue = Double.parseDouble(splitJoinResult(inputTextView.getText().toString(), ",", "."));
         switch (currentOperator) {
             case "+":
-                currentResult += value;
+                currentResult += currentValue;
                 break;
             case "-":
-                currentResult -= value;
+                currentResult -= currentValue;
                 break;
             case "*":
-                currentResult *= value;
+                currentResult *= currentValue;
                 break;
             case "÷":
-                if (value != 0) {
-                    currentResult /= value;
+                if (currentValue != 0) {
+                    currentResult /= currentValue;
                 } else {
-//                    isNewInput = true;
+                    isNewInput = true;
                     divideByZero = true;
                 }
                 break;
         }
     }
 
-    private void displayResult(boolean isClickEqual, boolean divideByZero) {
-        if (divideByZero) {
-            resultTextView.setText(R.string.handleDivisionByZero);
-        } else {
-            String[] res = String.valueOf(currentResult).split("\\.");
-            System.out.println(Arrays.toString(res));
-            String re = "";
+    private String splitJoinResult(String result, String splitOperator, String joinOperator) {
+        String[] res = result.split(splitOperator);
+        System.out.println(Arrays.toString(res));
+        String re = result;
+        if (res.length >= 2) {
             if (res[1].equals("0") || res[1].equals("00")) {
                 re = res[0];
             } else {
-                re = String.format("%s,%s", res[0], res[1]);
+                re = String.format("%s%s%s", res[0], joinOperator, res[1]);
             }
-            resultTextView.setText(isClickEqual ? "= " + re : re);
+        }
+        return re;
+    }
+
+    private void displayResult(boolean divideByZero) {
+        if (divideByZero) {
+            inputTextView.setText(R.string.handleDivisionByZero);
+        } else {
+            String re = splitJoinResult(String.valueOf(currentResult), "\\.", ",");
+            inputTextViewOperation.setText(String.format("%s%s=", inputTextViewOperation.getText().toString(), inputTextView.getText().toString()));
+            inputTextView.setText(re);
         }
 
     }
